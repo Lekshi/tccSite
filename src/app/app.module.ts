46a5c3ecf28648094cad09a02555841e44186c59
Gustavo Rodrigues <gustavogr1806@gmail.com>
2023-06-18 19:00:23 -0300

    finalização do site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
